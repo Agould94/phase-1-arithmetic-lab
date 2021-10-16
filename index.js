ed160ee4edcdbd65e541828ab83f273e803ef4b4
Alexander Gould <alexander.gould94@gmail.com>
2021-10-16 16:57:51 -0700

    commit tree f84d4e2a364ff0cdbac8623c37bba2ea4653dccc

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1*num2
 
-// Write your code here
+let random = Math.floor(Math.random()*1000)+1
+
+const num3=64
+const num4=5
+let mod = num3%num4
+
+let max = Math.max(1,4,5,2,6,20,4,6,7)
